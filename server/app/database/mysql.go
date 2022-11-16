353bbb6c01506c232a306b304bbed04a250e82c8
jiangxia <963798512@qq.com>
2022-11-16 22:05:55 +0800

    feat: gorm 数据库配置

--- server/app/database/mysql.go
+++ server/app/database/mysql.go
@@ -2,13 +2,17 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"gitee.com/jiang-xia/gin-zone/server/pkg/utils"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
-var Mysql *gorm.DB
+var Mysql *gorm.DB // gorm数据库实例
 
 func init() {
 	dbSetup()
@@ -39,40 +43,31 @@ func dbSetup() {
 	// dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	//拼接下dsn参数, dsn格式可以参考上面的语法，这里使用Sprintf动态拼接dsn参数，因为一般数据库连接参数，我们都是保存在配置文件里面，需要从配置文件加载参数，然后拼接dsn。
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbName)
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       dsn,   // DSN data source name
-		DefaultStringSize:         256,   // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
-	}), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(db)
-	// 设置默认表名的命名规则
-	// gorm.DefaultTableNameHandler = func(Mysql *gorm.DB, defaultTableName string) string {
-	// 	return tablePrefix + defaultTableName
-	// }
+	Mysql, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// 命名策略
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   tablePrefix, // 自定义表名前缀
+			SingularTable: true,        // 全局禁用表名复数
 
-	// Mysql.SingularTable(true)                      // 全局禁用表名复数
-	// Mysql.DB().SetMaxIdleConns(25)                 // 设置最大空闲连接数
-	// Mysql.DB().SetMaxOpenConns(100)                // 设置最大连接数
-	// Mysql.DB().SetConnMaxLifetime(5 * time.Minute) // 设置每个链接的过期时间
+		},
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+	sqlDB, err := Mysql.DB()
+	sqlDB.SetMaxIdleConns(25)                 // 设置最大空闲连接数
+	sqlDB.SetMaxOpenConns(100)                // 设置最大连接数
+	sqlDB.SetConnMaxLifetime(5 * time.Minute) // 设置每个链接的过期时间
 
-	// 启用Logger，显示详细日志
-	// Mysql.LogMode(true)
-
+	fmt.Println(Mysql, err)
 	// 调试单个操作，显示此操作的详细日志
-	// Mysql.Debug().Where("name = ?", "jinzhu").First(&User{})
+	// db.Debug().Where("name = ?", "jinzhu").First()
 }
 
 func CloseDB() {
 	defer func() {
-		// err := Mysql.Close()
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		sqlDB, err := Mysql.DB()
+		sqlDB.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}()
 }
--- server/go.mod
+++ server/go.mod
@@ -35,7 +35,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
-	github.com/go-sql-driver/mysql v1.6.0
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/json-iterator/go v1.1.12 // indirect
