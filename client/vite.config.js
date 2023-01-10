b27c9e0ff9aa43a11643f95b5e9cf6ae340f5db3
jiangxia <963798512@qq.com>
2023-01-11 00:29:40 +0800

    fix：1.client pwa配置修复

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,35 +1,39 @@
-import {
-	VitePWA
-} from 'vite-plugin-pwa'
-import {
-	defineConfig
-} from 'vite';
-import uni from '@dcloudio/vite-plugin-uni';
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
 
 export default defineConfig({
-	plugins: [
-		uni(),
-		// #ifdef H5 || APP-PLUS
-		VitePWA({
-			"registerType": 'autoUpdate',
-			"name": "zone",
-			"short_name": "zone",
-			"icons": [
-			    {
-			        "src": "/android-chrome-192x192.png",
-			        "sizes": "192x192",
-			        "type": "image/png"
-			    },
-			    {
-			        "src": "/android-chrome-512x512.png",
-			        "sizes": "512x512",
-			        "type": "image/png"
-			    }
-			],
-			"theme_color": "#ffffff",
-			"background_color": "#ffffff",
-			"display": "standalone"
-		})
-		// #endif
-	],
+  plugins: [
+    uni(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "zone",
+        short_name: "zone",
+        icons: [
+          {
+            src: "./static/logo/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "./static/logo/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "./static/logo/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
+      },
+    }),
+  ],
 });
