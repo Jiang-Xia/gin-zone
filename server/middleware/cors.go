720fceee7f4d8584ea3e6d3e00cfa111df882d7b
jiangxia <963798512@qq.com>
2023-02-16 23:26:02 +0800

    feat:  1.server 响应头

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -15,7 +15,7 @@ func Cors() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Headers", "Origin,Content-Type,Authorization")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT,PATCH")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "false")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		// 放行所有OPTIONS方法
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
