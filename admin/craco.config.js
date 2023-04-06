878285883529f8657bcb142dc78ec19dc9ebc320
jiang-xia <963798512@qq.com>
2023-04-06 16:28:05 +0800

    style:  1.整体代码风格配置 2.admin基本配置

--- admin/craco.config.js
+++ admin/craco.config.js
@@ -1,3 +1,34 @@
+const path = require('path');
+const { name } = require('./package.json');
+
+const pathResolve = pathUrl => path.join(__dirname, pathUrl);
+
 module.exports = {
-    // ...
-  };
+  reactScriptsVersion: 'react-scripts' /* (default value) */,
+  webpack: {
+    alias: {
+      '@': pathResolve('src'),
+      '@assets': pathResolve('src/assets'),
+      '@components': pathResolve('src/components'),
+    },
+    configure(webpackConfig) {
+      // 配置扩展扩展名
+      webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
+      // 接入微前端框架qiankun的配置,不接入微前端可以不需要
+      // webpackConfig.output.library = `${name}-[name]`;
+      // webpackConfig.output.libraryTarget = 'umd';
+      // webpackConfig.output.globalObject = 'window';
+      return webpackConfig;
+    },
+  },
+  devServer: {
+    // 本地服务的端口号
+    port: 3001,
+    // 本地服务的响应头设置
+    headers: {
+      // 允许跨域
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+};
+
--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import type { FC } from 'react';
-import { Button } from 'antd';
-import { ConfigProvider } from 'antd';
 import 'antd/dist/reset.css';
+import Container from './layout/Container';
 import './App.css';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Container />
   </div>
 );
-const defaultLayout = () => (
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#00b96b',
-      },
-    }}
-  >
-    <App />
-  </ConfigProvider>
-);
-export default defaultLayout
+export default App;
